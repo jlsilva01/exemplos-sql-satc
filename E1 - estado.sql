3b817c05c6a693f6fbfbbe4a97100cd81b3796b4
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-10-03 11:22:59 -0300

    Add files via upload

---
+++ E1 - estado.sql
@@ -0,0 +1,30 @@
+
+/* INSERE estadoS */
+
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (12, 1, 'Acre', 'AC');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (27, 2, 'Alagoas', 'AL');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (16, 1, 'Amapá', 'AP');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (13, 1, 'Amazonas', 'AM');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (29, 2, 'Bahia', 'BA');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (23, 2, 'Ceará', 'CE');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (53, 5, 'Distrito Federal', 'DF');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (32, 3, 'Espírito Santo', 'ES');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (52, 5, 'Goiás', 'GO');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (21, 2, 'Maranhão', 'MA');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (51, 5, 'Mato Grosso', 'MT');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (50, 5, 'Mato Grosso do Sul', 'MS');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (31, 3, 'Minas Gerais', 'MG');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (15, 1, 'Pará', 'PA');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (25, 2, 'Paraíba', 'PB');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (41, 4, 'Paraná', 'PR');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (26, 2, 'Pernambuco', 'PE');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (22, 2, 'Piauí', 'PI');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (33, 3, 'Rio de Janeiro', 'RJ');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (24, 2, 'Rio Grande do Norte', 'RN');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (43, 4, 'Rio Grande do Sul', 'RS');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (11, 1, 'Rondônia', 'RO');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (14, 1, 'Roraima', 'RR');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (42, 4, 'Santa Catarina', 'SC');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (35, 3, 'São Paulo', 'SP');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (28, 2, 'Sergipe', 'SE');
+INSERT INTO estado (cd_estado, cd_regiao, nm_estado, sigla_uf) VALUES (17, 1, 'Tocantins', 'TO');
---
+++ E1 - regiao.sql
@@ -0,0 +1,8 @@
+
+/* INSERE REGIOES */
+
+INSERT INTO regiao (cd_regiao, nm_regiao) values (1, 'NORTE');
+INSERT INTO regiao (cd_regiao, nm_regiao) values (2, 'NORDESTE');
+INSERT INTO regiao (cd_regiao, nm_regiao) values (3, 'SUDESTE');
+INSERT INTO regiao (cd_regiao, nm_regiao) values (4, 'SUL');
+INSERT INTO regiao (cd_regiao, nm_regiao) values (5, 'CENTRO-OESTE');
